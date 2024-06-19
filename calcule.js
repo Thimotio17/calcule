ca3d7e1eee2c38f3ba59a896cd494e931c97c8b5
Luis Eduardo Silverio Proencio <luis.eduardo.proencio@gmail.com>
2024-06-19 08:59:27 -0300

    melhorando o código adicionando MDC

--- calcule.js
+++ calcule.js
@@ -1,7 +1,7 @@
 let a = [];
 let b = [];
 
-console.log("Digite 2 numeros pra calcular mmc");
+console.log("Digite 2 números para calcular MMC e MDC:");
 
 process.stdin.on("data", function(data) {
     let entrada = Number(data.toString().trim());
@@ -20,16 +20,26 @@ process.stdin.on("data", function(data) {
         let num1 = a[0];
         let num2 = b[0];
 
-        let max = (num1 > num2) ? num1 : num2;
-
-        while (true) {
-            if (max % num1 === 0 && max % num2 === 0) {
-                console.log(`O MMC de ${num1} e ${num2} é: ${max}`);
-                break;
+        // Função para calcular o MDC
+        function calcularMDC(a, b) {
+            while (b !== 0) {
+                let temp = b;
+                b = a % b;
+                a = temp;
             }
-            max++;
+            return a;
         }
+
+        const calcularMMC = (a, b) => (a * b) / calcularMDC(a, b);
+
+        let mdc = calcularMDC(num1, num2);
+        let mmc = calcularMMC(num1, num2);
+
+        console.log(`O MDC de ${num1} e ${num2} é: ${mdc}`);
+        console.log(`O MMC de ${num1} e ${num2} é: ${mmc}`);
 
         process.exit();
     }
+      
+    
 });
